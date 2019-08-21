4159499b36b4652368c959480605ca29afdf04ba
Derek Chiu <dchiu@bccrc.ca>
2019-08-21 15:50:53 -0700

    add output_dir as TextInput widget

--- R/use_rsf.R
+++ R/use_rsf.R
@@ -11,20 +11,8 @@
 #' @export
 use_rsf <- function(path, ...) {
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
-
-  dots <- list(...)
-  text <- lapply(seq_along(dots), function(i) {
-    key <- names(dots)[[i]]
-    val <- dots[[i]]
-    paste0(key, ": ", val)
-  })
-
-  contents <- paste(
-    paste(text, collapse = "\n"),
-    sep = "\n"
-  )
-
-  write_bookdown_yml(path)
+  params <- list(...)
+  write_bookdown_yml(path, params)
   write_output_yml(path)
   write_index(path)
   write_src(path)
--- R/write_bookdown_yml.R
+++ R/write_bookdown_yml.R
@@ -1,10 +1,12 @@
 # _bookdown.yml
-write_bookdown_yml <- function(path) {
-  bookdown_yml <- list(
-    book_filename = paste(basename(path), "RSF", sep = "_"),
-    output_dir = "reports",
-    rmd_subdir = "src",
-    before_chapter_script = "src/utils.R"
+write_bookdown_yml <- function(path, params) {
+  bookdown_yml <- c(
+    list(
+      book_filename = paste(basename(path), "RSF", sep = "_"),
+      rmd_subdir = "src",
+      before_chapter_script = "src/utils.R"
+    ),
+    params
   )
   yaml::write_yaml(
     x = bookdown_yml,
