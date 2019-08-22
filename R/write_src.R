c737382ce33779f59b786767d17473685f59308b
Derek Chiu <dchiu@bccrc.ca>
2019-08-22 10:23:41 -0700

    add write_rmd helper

--- R/write_src.R
+++ R/write_src.R
@@ -3,7 +3,25 @@ write_src <- function(path) {
   src_path <- file.path(path, "src")
   dir.create(src_path)
   write_utils(src_path)
-  writeLines(text = "# Introduction", con = file.path(src_path, "01-introduction.Rmd"))
-  writeLines(text = "# Methods", con = file.path(src_path, "02-methods.Rmd"))
-  writeLines(text = "# Results", con = file.path(src_path, "03-results.Rmd"))
+  write_rmd(src_path, "Introduction", "01-introduction.Rmd")
+  write_rmd(src_path, "Methods", "02-methods.Rmd")
+  write_rmd(src_path, "Results", "03-results.Rmd")
+}
+
+# utils
+write_utils <- function(path) {
+  chunk_opts <- 'knitr::opts_chunk$set(
+  echo = FALSE,
+  message = FALSE,
+  warning = FALSE,
+  results = "asis",
+  fig.align = "center"
+)'
+  writeLines(text = c("# knitr options", chunk_opts),
+             con = file.path(path, "utils.R"))
+}
+
+# Rmd
+write_rmd <- function(path, header, rmd_name) {
+  writeLines(text = paste("#", header), con = file.path(path, rmd_name))
 }
