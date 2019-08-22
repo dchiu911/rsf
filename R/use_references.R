9ebf931f7ae53e378734f883d8dabe80bdc7462f
Derek Chiu <dchiu@bccrc.ca>
2019-08-22 12:05:28 -0700

    check if refs exist, add usethis console feedback

--- R/use_references.R
+++ R/use_references.R
@@ -5,6 +5,12 @@
 #' @param number number prepended to references Rmd source file.
 #' @export
 use_references <- function(number = 99) {
+  # Check if references already exists
+  if ("bibliography" %in% names(yaml::read_yaml(here::here("index.Rmd")))) {
+    usethis::ui_done("References already added")
+    return(invisible())
+  }
+
   # Create references Rmd source file
   src_path <- here::here("src")
   rmd_name <- paste0(stringr::str_pad(number, 2, pad = 0), "-references.Rmd")
@@ -19,12 +25,14 @@ use_references <- function(number = 99) {
     text = c(header, chunk_start, pkg_list, chunk_end),
     con = file.path(src_path, rmd_name)
   )
+  usethis::ui_done(paste(rmd_name, "added"))
 
   # Call packages.bib from index.Rmd
   index_rmd <- readLines(here::here("index.Rmd"))
   before_style <- grep("biblio-style", index_rmd) - 1
   index_rmd <- append(index_rmd, "bibliography: packages.bib", before_style)
   writeLines(text = index_rmd, con = here::here("index.Rmd"))
+  usethis::ui_done("index.Rmd modified")
 
   # Add bib options to bookdown::pdf_book output
   output_yml <- yaml::read_yaml(here::here("_output.yml"))
@@ -32,10 +40,12 @@ use_references <- function(number = 99) {
   output_yml[["bookdown::pdf_book"]] <-
     c(output_yml[["bookdown::pdf_book"]], bib_opts)
   yaml::write_yaml(x = output_yml, file = here::here("_output.yml"))
+  usethis::ui_done("_output.yml modified")
 
   # Rename Bibliography to References in tex
   preamble_tex <- readLines(here::here("preamble.tex"))
   bib_rename <- "\n\\renewcommand{\\bibname}{References}
 % https://tex.stackexchange.com/questions/12597/renaming-the-bibliography-page-using-bibtex"
   writeLines(text = c(preamble_tex, bib_rename), con = here::here("preamble.tex"))
+  usethis::ui_done("preamble.tex modified")
 }
