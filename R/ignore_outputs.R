6f32098144a05199e908050ae3f29a68f3bc0afe
Derek Chiu <dchiu@bccrc.ca>
2019-08-22 14:08:39 -0700

    add ignore_ouputs()

---
+++ R/ignore_outputs.R
@@ -0,0 +1,20 @@
+#' Git ignore generated outputs
+#'
+#' Add generated reports, figures, and HTML libraries to .gitignore
+#'
+#' @param figs_only logical; if `TRUE` (default), only figures are git ignored
+#' @export
+git_ignore_outputs <- function(figs_only = TRUE) {
+  git_ignore <- readLines(here::here(".gitignore"))
+  bookdown_yml <- yaml::read_yaml(here::here("_bookdown.yml"))
+  output_dir <- bookdown_yml[["output_dir"]]
+  if (figs_only) {
+    output_dir <- file.path(output_dir, paste0(bookdown_yml[["book_filename"]], "_files"))
+  }
+  outputs <- c(output_dir, "_bookdown_files")
+  if (all(outputs %in% git_ignore)) {
+    usethis::ui_done("Outputs already git ignored")
+    return(invisible())
+  }
+  usethis::use_git_ignore(outputs)
+}
