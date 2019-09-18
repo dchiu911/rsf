02f78bbf587185572e842c8338c1fad6b602407d
Derek Chiu <dchiu@bccrc.ca>
2019-09-18 14:34:13 -0700

    don't use ruled table captions

--- R/write_preamble.R
+++ R/write_preamble.R
@@ -1,16 +1,7 @@
 # preamble.tex
 write_preamble <- function(path) {
   cmds <-
-    "\\usepackage{booktabs}
-\\usepackage{pdflscape}
-\\newcommand{\\blandscape}{\\begin{landscape}}
-\\newcommand{\\elandscape}{\\end{landscape}}
-\\usepackage{float}
-\\floatplacement{figure}{H}
-\\floatstyle{ruled}
-\\restylefloat{figure}
-
-\\let\\paragraph\\oldparagraph
+    "\\let\\paragraph\\oldparagraph
 \\let\\subparagraph\\oldsubparagraph
 
 \\usepackage{titlesec, blindtext, color}
