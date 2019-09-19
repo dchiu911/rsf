e2a1df05f45e7cb82722370d2894a05c03456d65
Derek Chiu <dchiu@bccrc.ca>
2019-09-19 16:02:37 -0700

    don't force opening index.Rmd

--- R/write_index.R
+++ R/write_index.R
@@ -15,7 +15,8 @@ write_index <- function(path) {
       biblio_style = "apalike"
     ) %>%
     ymlthis::yml_citations(link_citations = TRUE)
-  ymlthis::use_rmarkdown(index_yml, file.path(path, "index.Rmd"),
-                         body = "\n# Preface {-}",
-                         quiet = TRUE)
+  writeLines(
+    text = c(utils::capture.output(print(index_yml)), "\n# Preface {-}"),
+    con = file.path(path, "index.Rmd")
+  )
 }
