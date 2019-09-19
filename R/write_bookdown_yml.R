b664fd11b30a6f3c9af273b4c5146b8f5775b1b1
Derek Chiu <dchiu@bccrc.ca>
2019-09-19 12:47:44 -0700

    write bookdown.yml using ymlthis

--- R/write_bookdown_yml.R
+++ R/write_bookdown_yml.R
@@ -1,15 +1,13 @@
-# _bookdown.yml
+#' Write `_bookdown.yml`
+#' @noRd
 write_bookdown_yml <- function(path, params) {
-  bookdown_yml <- c(
-    list(
-      book_filename = paste(basename(path), "RSF", sep = "_"),
-      rmd_subdir = "src",
-      before_chapter_script = "src/utils.R"
-    ),
-    params
+  bookdown_yml <- rlang::exec(
+    ymlthis::yml_bookdown_opts,
+    .yml = ymlthis::yml_empty(),
+    book_filename = paste(basename(path), "RSF", sep = "_"),
+    rmd_subdir = "src",
+    before_chapter_script = "src/utils.R",
+    !!!params
   )
-  yaml::write_yaml(
-    x = bookdown_yml,
-    file = file.path(path, "_bookdown.yml")
-  )
+  ymlthis::use_bookdown_yml(bookdown_yml, path)
 }
