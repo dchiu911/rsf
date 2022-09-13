779dba3ccf10f581be71e17c9c54e146eb3748f4
Derek Chiu <dchiu@bccrc.ca>
2022-09-13 16:00:34 -0700

    add project options to initialize with git and/or renv

--- R/use_rsf.R
+++ R/use_rsf.R
@@ -8,17 +8,20 @@
 #' and output directory can be specified.
 #'
 #' @param path project path
-#' @param ... additional configurations for `_bookdown.yml` as described in
-#'   <https://bookdown.org/yihui/bookdown/configuration.html>. Currently, only
-#'   `output_dir` is supported.
+#' @param ... project configurations supported:
+#' * `output_dir` in `_bookdown.yml` <https://bookdown.org/yihui/bookdown/configuration.html>
+#' * initialize git repo via [gert::git_init()]
+#' * initialize renv via [renv::init()]
 #' @export
 use_rsf <- function(path, ...) {
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
   params <- list(...)
-  write_bookdown_yml(path, params)
+  write_bookdown_yml(path, params$output_dir)
   write_output_yml(path)
   write_index(path)
   write_src(path)
   write_preamble(path)
   write_gitignore(path)
+  if (params$git) gert::git_init(path)
+  if (params$renv) renv::init(path, restart = FALSE)
 }
--- R/write_bookdown_yml.R
+++ R/write_bookdown_yml.R
@@ -1,13 +1,13 @@
 #' Write `_bookdown.yml`
 #' @noRd
-write_bookdown_yml <- function(path, params) {
+write_bookdown_yml <- function(path, output_dir) {
   bookdown_yml <- rlang::exec(
     ymlthis::yml_bookdown_opts,
     .yml = ymlthis::yml_empty(),
     book_filename = paste(basename(path), "RSF", sep = "_"),
     before_chapter_script = "src/utils.R",
     rmd_subdir = "src",
-    !!!params
+    output_dir = output_dir
   )
   options(ymlthis.remove_blank_line = TRUE)
   ymlthis::use_bookdown_yml(.yml = bookdown_yml, path = path, quiet = TRUE)
